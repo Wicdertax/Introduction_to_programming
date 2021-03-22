9fff7102f92aedcc6589dd323ebcf99714f204ea
Juan Sebastian Leal Pinzon <juan.leal@usantoto.edu.co>
2021-03-22 15:42:31 -0500

    upload
    upload

---
+++ Talleres primer periodo/JAVA/P1T36_SYSTEM.OUT.PRINTLN/src/usta/fis/Main.java
@@ -0,0 +1,25 @@
+package usta.fis;
+//AUTHOR: Juan Sebastian Leal Pinzon
+//Date: 19-march-2021
+//Description: This program show a Seahorse with symbols
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("-----------$$$$--");
+        System.out.println("---------$$$$$$$$");
+        System.out.println("-------$$$$$$$$$$$$$");
+        System.out.println("-----$$$$$$$$$$$$$$$$");
+        System.out.println("----------$$$$--");
+        System.out.println("----------$$$$--");
+        System.out.println("----------$$$$--");
+        System.out.println("----------$$$$--");
+        System.out.println("----------$$$$--");
+        System.out.println("----------$$$$--");
+        System.out.println("----------$$$$--");
+        System.out.println("----------$$$$--");
+        System.out.println("----------$$$$--");
+        System.out.println("----------$$$$--");
+
+    }
+}
