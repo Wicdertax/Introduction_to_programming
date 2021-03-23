10da069cea5ca2314734615931a9302a96372118
Juan Sebastian Leal Pinzon <juan.leal@usantoto.edu.co>
2021-03-23 08:13:58 -0500

    upload

--- Talleres primer periodo/JAVA/P1T36_SYSTEM.OUT.PRINTLN/src/usta/fis/Main.java
+++ Talleres primer periodo/JAVA/P1T36_SYSTEM.OUT.PRINTLN/src/usta/fis/Main.java
@@ -6,20 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("-----------$$$$--");
-        System.out.println("---------$$$$$$$$");
-        System.out.println("-------$$$$$$$$$$$$$");
-        System.out.println("-----$$$$$$$$$$$$$$$$");
-        System.out.println("----------$$$$--");
-        System.out.println("----------$$$$--");
-        System.out.println("----------$$$$--");
-        System.out.println("----------$$$$--");
-        System.out.println("----------$$$$--");
-        System.out.println("----------$$$$--");
-        System.out.println("----------$$$$--");
-        System.out.println("----------$$$$--");
-        System.out.println("----------$$$$--");
-        System.out.println("----------$$$$--");
+        System.out.println("________$$$$..");
+        System.out.println("______$$$$$$$$$");
+        System.out.println("______$$$$$$$_$");
+        System.out.println("_____$$$$$$$$$$");
+        System.out.println("______$$$$$$$$$$");
+        System.out.println("_____$$$$$$_$$$$$");
+        System.out.println("____$$$$$$$_____$$$");
+        System.out.println("____$$$$$$$$_____$");
+        System.out.println("____$$$$$$$$$$");
+        System.out.println("_____$$$$$$$$$$");
+        System.out.println("_____$$$$$$$$$$$");
+        System.out.println("______$$$$$$$$$$$");
+        System.out.println("_$$$$___$$$$$$$$$");
+        System.out.println("__$$$$$$$$$$$$$$$");
+        System.out.println("_$$$$$$$$$$$$$$$");
+        System.out.println("__$$$$$$$$$$$$$");
+        System.out.println("$$$$$$$$$$$$$");
+        System.out.println("__$__$$$$$$");
+        System.out.println("____$$$$$$");
+        System.out.println("____$$$$$");
+        System.out.println("___$$$$$$_____$");
+        System.out.println("___$$$$$$___$$_$$");
+        System.out.println("____$$$$$___$__$$");
+        System.out.println("____$$$$$______$$");
+        System.out.println("_____$$$$$____$$$");
+        System.out.println("_______$$$$$$$$$");
+        System.out.println("__________$$$$");
 
     }
 }
