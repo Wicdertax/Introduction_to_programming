665930c017c28ddcb4d0db69b12bf091a0b05bb6
Juan Sebastian Leal Pinzon <juan.leal@usantoto.edu.co>
2021-04-19 07:10:41 -0500

    upload

--- Talleres primer periodo/JAVA/P1T42_Calculator_V2/src/usta/fis/Main.java
+++ Talleres primer periodo/JAVA/P1T42_Calculator_V2/src/usta/fis/Main.java
@@ -13,9 +13,9 @@ public class Main {
 	v_number_one = keyboard.nextInt();
 	System.out.println("Please input the number two");
 	v_number_two = keyboard.nextInt();
-	System.out.println("Please input 1 to sum, 2 to subtract, 3 multiply, 4 division");
+	System.out.println("Please input 1 to sum, 2 to subtract, 3 multiply, 4 division, o exit");
 	v_operation = keyboard.nextInt();
-	System.out.println("The result of the operation is "+ p_operate_numbers(v_number_one,v_number_two,v_operation));
+		System.out.println("The result of the operation is " + p_operate_numbers(v_number_one, v_number_two, v_operation));
 
     }
     public static void p_show_info_program(){
@@ -27,20 +27,19 @@ public class Main {
 	}
 	public static int p_operate_numbers(int v_number_one,int v_number_two,int v_operation){
 		Scanner keyboard = new Scanner(System.in);
-    	while(v_operation<=0 || v_operation>=5) {
+    	while(v_operation<=-1 || v_operation>=5) {
     		System.err.println("The value can't be less than one and greater than 5, please input again 1 to sum, 2 to subtract, 3 multiply, 4 division");
 			v_operation = keyboard.nextInt();
 		}
-			if (v_operation == 1) {
-				return v_number_one + v_number_two;
-			}
-			if (v_operation == 2) {
-				return v_number_one - v_number_two;
-			}
-			if (v_operation == 3) {
-				return v_number_one * v_number_two;
-			} else {
-				return v_number_one / v_number_two;
-			}
+		switch(v_operation) {
+		case 1:
+			return v_number_one + v_number_two;
+		case 2:
+			return v_number_one - v_number_two;
+		case 3:
+			return v_number_one * v_number_two;
+		default:
+			return v_number_one / v_number_two;
+	}
 	}
 }
