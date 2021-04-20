6eae99d074f7cede7165d0bef4adc731996c1d47
Juan Sebastian Leal Pinzon <juan.leal@usantoto.edu.co>
2021-04-19 22:44:33 -0500

    upload

--- Talleres primer periodo/JAVA/P1T42_Calculator_V2/src/usta/fis/Main.java
+++ Talleres primer periodo/JAVA/P1T42_Calculator_V2/src/usta/fis/Main.java
@@ -6,31 +6,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	int v_number_one=0,v_number_two=0,v_operation=0;
+	int v_number_one=0,v_number_two,v_operation;
 	p_show_info_program();
 	Scanner keyboard = new Scanner(System.in);
-
-		do {
-			System.out.println("Please input 1 to sum, 2 to subtract, 3 multiply, 4 division, 5 prime number or not prime number, 0 exit");
+	do {
+		System.out.println("Please input 1 to sum, 2 to subtract, 3 multiply, 4 division, 5 prime number or not prime number, 0 exit");
+		v_operation = keyboard.nextInt();
+		while (v_operation <= -1 || v_operation >= 6) {
+			System.err.println("The number can't be less than 0 and greater than 5, please input again a number between 0 and 5");
+			System.err.println("Please input 1 to sum, 2 to subtract, 3 multiply, 4 division, 5 prime number or not prime number, o exit");
 			v_operation = keyboard.nextInt();
-			while (v_operation<=-1|| v_operation>=6) {
-				System.err.println("The number can't be less than 0 and greater than 5, please input again a number between 0 and 5");
-				System.err.println("Please input 1 to sum, 2 to subtract, 3 multiply, 4 division, 5 prime number or not prime number, o exit");
-				v_operation = keyboard.nextInt();
-			}
-			if (v_operation != 5) {
-				System.out.println("---------------------------");
-				System.out.println("     Basic Calculator");
-				System.out.println("---------------------------");
-				System.out.println("Please input the number one");
-				v_number_one = keyboard.nextInt();
-				System.out.println("Please input the number two");
-				v_number_two = keyboard.nextInt();
-				System.out.println("The result of the operation is " + p_operate_numbers( v_number_one, v_number_two, v_operation));
-			}else{
+		}
+		if (v_operation != 5) {
+			System.out.println("---------------------------");
+			System.out.println("     Basic Calculator");
+			System.out.println("---------------------------");
+			System.out.println("Please input the number one");
+			v_number_one = keyboard.nextInt();
+			System.out.println("Please input the number two");
+			v_number_two = keyboard.nextInt();
+			System.out.println("The result of the operation is " + p_operate_numbers(v_number_one, v_number_two, v_operation));
+		} else {
 			System.out.println("The number " + p_prime_numbers(v_number_one));
-			}
-		}while(v_operation!=0);
+		}
+	} while (v_operation != 0);
+
     }
     public static void p_show_info_program(){
 		System.out.println("╔════════════════════════════════════╗");
@@ -41,8 +41,8 @@ public class Main {
 	}
 	public static String p_operate_numbers(int v_number_one,int v_number_two,int v_operation){
 		Scanner keyboard = new Scanner(System.in);
-		int v_result=0;
-		String v_result_save=" ";
+		int v_result;
+		String v_result_save;
     	while(v_operation<=-1 || v_operation>5) {
     		System.err.println("The value can't be less than one and greater than 5, please input again 1 to sum, 2 to subtract, 3 multiply, 4 division");
 			v_operation = keyboard.nextInt();
