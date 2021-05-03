2472d27c9239c6b7195176f6324a1389437832db
Juan Sebastian Leal Pinzon <juan.leal@usantoto.edu.co>
2021-05-03 07:18:33 -0500

    upload

--- Talleres primer periodo/JAVA/Fibonacci/src/usta/fis/Main.java
+++ Talleres primer periodo/JAVA/Fibonacci/src/usta/fis/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        p_show_info_program();
         int serie=0;
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please input the number limit of the series");
@@ -11,7 +12,7 @@ public class Main {
         int fibo_array[]= new int[100];
         fibo_array=serie_fibonacci(serie);
         System.out.println("The fibonacci series for the number "+serie+" are: ");
-
+        System.out.println(0);
         for (int i = 0; i < serie; i++) {
             if (fibo_array[i]>0) {
                 System.out.println(fibo_array[i]);
@@ -27,5 +28,12 @@ public class Main {
             fibo_array[i+1] = fibo_array[i-1] + fibo_array[i];
         }
         return fibo_array;
+    }
+    public static void p_show_info_program(){
+        System.out.println("---------------------------------");
+        System.out.println("Serie Fibonacci v 1.0");
+        System.out.println("Maker: Juan Sebastian Leal Pinzon");
+        System.out.println("Date: 25 April 2021");
+        System.out.println("---------------------------------");
     }
 }
